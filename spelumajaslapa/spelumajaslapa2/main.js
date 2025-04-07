89448e65defa4dc07d9fc00a3bdaa5a88a0843db
Cipsis1 <152157968+Cipsis1@users.noreply.github.com>
2025-04-07 11:14:20 +0300

    Add files via upload

---
+++ spelumajaslapa/spelumajaslapa2/main.js
@@ -0,0 +1,28 @@
+function laiks(){
+    const now = new Date();
+    const formatetsLaiks = now.getFullYear() + "-" +
+    String(now.getMonth()+1).padStart(2, '0') + "-" +
+    String(now.getDate()).padStart(2, '0')+ " " +
+    String(now.getHours()).padStart(2, '0')+ ":" +
+    String(now.getMinutes()).padStart(2, '0')+ ":" +
+    String(now.getSeconds()).padStart(2, '0');
+   
+    document.getElementById("laiks").textContent = formatetsLaiks;
+  }
+   
+  laiks();
+  setInterval(laiks,1000)
+  
+    let text;
+    let person;
+    if(window.localStorage.getItem("person") == null){
+      person = prompt("Please enter your name (max 20 characters):");
+      window.localStorage.setItem("person", person);
+    }
+    else{
+      person = window.localStorage.getItem("person");
+    }
+    text = person.substring(0, 20); 
+    document.getElementById("alert").innerHTML = text;
+    
+    //localStorage.clear();
